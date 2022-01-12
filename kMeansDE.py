5c9502b9891d08a7482e142c458ddce0da58d08c
mistretta_marco <marco.mistretta@stud.unifi.it>
2022-01-12 19:05:03 +0100

    clean up

--- kMeansDE.py
+++ kMeansDE.py
@@ -1,36 +1,38 @@
 import math
 import random
-from array import array
-from itertools import count
 
 import numpy as np  # For data management
 import pandas as pd  # For data management
-from numpy.ma import count_masked
-from sklearn.metrics import pairwise_distances
-from sklearn.preprocessing import StandardScaler  # To transform the dataset
-from sklearn.cluster import KMeans  # To instantiate, train and use model
 from scipy.spatial import distance
 from sklearn.base import BaseEstimator, ClusterMixin
+from sklearn.cluster import KMeans  # To instantiate, train and use model
+from sklearn.metrics import pairwise_distances
+from sklearn.preprocessing import StandardScaler  # To transform the dataset
+
 
 class Solution:
     def __init__(self, cluster_centers, label, data):
         self.cluster_centers = cluster_centers
         self.label = label
-        self.ss_distance = np.sum([distance.pdist([cluster_centers[i], data[label == i][j]], 'sqeuclidean') for i in range(len(cluster_centers)) for j in range(len(data[label == i]))])
+        self.ss_distance = np.sum([distance.pdist([cluster_centers[i], data[label == i][j]], 'sqeuclidean') for i in
+                                   range(len(cluster_centers)) for j in range(len(data[label == i]))])
 
 
 class KMeansDE(BaseEstimator, ClusterMixin):
 
     def __init__(self, n_clusters=2, max_iter=5000, tol=1e-3,
-                 verbose=0, population_lenght=150, kmeans_max_iter=50000):
+                 verbose=0, population_lenght=150, kmeans_max_iter=5000, scaling=True, dataset_name="None"):
         self.n_clusters = n_clusters
         self.max_iter = max_iter
         self.tol = tol
+        self.scaling = scaling
+        self.dataset_name = dataset_name
         self.population_lenght = population_lenght
         self.verbose = verbose
         self.kmeans_max_iter = kmeans_max_iter
         self.population = None
         self.data = None
+        self.print_parameters()
 
     def _compute_crossover(self, parent_1, parent_2, parent_3):  # S1 + F(S2-S3)
         f = random.uniform(0.5, 0.8)
@@ -43,71 +45,78 @@ class KMeansDE(BaseEstimator, ClusterMixin):
 
     def _prepareDataset(self, data):
 
-        if self.verbose:
-            print("shape ", data.shape)
-
         data = pd.DataFrame(data)
 
         if self.verbose:
-            print("dati", data.head())
+            print("\nHead of dataset")
+            print(data.head())
 
-        scaler = StandardScaler()
-        scaled_array = scaler.fit_transform(data)
-        scaled_dataframe = pd.DataFrame(scaled_array, columns=data.columns)
+        if self.scaling:
+            print("\nScaling dataset...")
+            scaler = StandardScaler()
+            scaled_array = scaler.fit_transform(data)
+            data = pd.DataFrame(scaled_array, columns=data.columns)
+            if self.verbose:
+                print("Head of scaled dataset")
+                print(data.head())
+
         if self.verbose:
-            print("dati scalati", scaled_dataframe.head())
+            print("\nShape of dataset: ", data.shape)
 
         self.data = data.to_numpy()
 
+        print("\n******* Initialization *******")
         return
 
     def _initializeS(self):
         self.population = []
         for i in range(self.population_lenght):
             kmeans_model = KMeans(n_clusters=self.n_clusters, init='random', max_iter=1,
-                                  n_init=1, random_state=random.randint(1, 100000000))  # preparo l'algoritmo di kmeans
+                                  n_init=1)
 
             kmeans_model.fit(self.data)
             centroids = kmeans_model.cluster_centers_
             labels = kmeans_model.labels_
 
-            self.population.append(Solution(centroids,labels,self.data))
+            self.population.append(Solution(centroids, labels, self.data))
 
-        if self.verbose:
-            print("**** Initial population (initial centroids) ****\n", self.population)
+        if self.verbose == 2:
+            print("**** Initial population (initial centroids) ****\n")
+            for i in range(len(self.population)):
+                print("Cluster+" + str(i + 1) + "\n ", self.population[i].cluster_centers)
 
         return
 
     def _stopping_criterion(self, consecutive_else, pick_success):
         if not pick_success:
             if self.verbose:
-                print("Tutta la popolazione è uguale")
+                print("Second stopping criterion, population has converged\n")
             return False
 
         if self.verbose:
-            print("consec iter ", consecutive_else)
+            print("Consecutive iteration without improvement:", consecutive_else)
 
         if consecutive_else >= self.max_iter:
             if self.verbose:
-                print("Max Iter raggiunte")
+                print("First stopping criterion, reach maximum iteration limit\n")
             return False
 
-        sum = 0
+        adder = 0
 
         population_unique = self.unique()
         if len(population_unique) < len(self.population) and self.verbose:
-            print("Qualcosa è uguale", len(self.population) - len(population_unique))
+            print("Number of redundant population elements: ", len(self.population) - len(population_unique))
 
         for k in range(len(population_unique)):
-
-            sum += np.sum([np.abs(population_unique[k].ss_distance - population_unique[x].ss_distance) for x in range(k + 1, len(population_unique))])
+            adder += np.sum([np.abs(population_unique[k].ss_distance - population_unique[x].ss_distance) for x in
+                             range(k + 1, len(population_unique))])
 
         if self.verbose:
-            print("toll ", self.tol, "sum ", sum)
+            print("Objective tollerance:", self.tol, " Actual precision:", adder)
 
-        if sum <= self.tol:
+        if adder <= self.tol:
             if self.verbose:
-                print('Quella roba con la tolleranza')
+                print("Second stopping criterion, population has converged\n")
             return False
         else:
             return True
@@ -165,7 +174,7 @@ class KMeansDE(BaseEstimator, ClusterMixin):
                 best = trial
                 best_index = i
         if self.verbose:
-            print("la migliore soluzione è la: " + str(best_index + 1) + "-esima componente di S, ",
+            print("Best solution is the" + str(best_index + 1) + "-th element of the population, centroids: ",
                   self.population[best_index].cluster_centers)
         return self.population[best_index]
 
@@ -180,6 +189,7 @@ class KMeansDE(BaseEstimator, ClusterMixin):
         pick_success = True
         while self._stopping_criterion(consecutive_else, pick_success):
             count_iter += 1
+            print("\n\n******* Iteration-" + str(count_iter) + " *******")
 
             for i in range(self.population_lenght):
 
@@ -244,9 +254,24 @@ class KMeansDE(BaseEstimator, ClusterMixin):
 
         return population_unique
 
+    def print_parameters(self):
+        print("******* Starting KMeansDE *******")
+        print("Datset:", self.dataset_name)
+        if self.scaling:
+            print("Dataset will be scaled")
+        else:
+            print("Dataset will not be scaled")
+        print("Number of clusters:", self.n_clusters)
+        print("Tolerance:", self.tol)
+        print("Size of population:", self.population_lenght)
+        print("Fit maximum iteration limit:", self.max_iter)
+        print("KMeans subroutine maximum iteration limit:", self.kmeans_max_iter)
+        print()
+
 
 if __name__ == '__main__':
-    bupa_data = np.loadtxt("bupa.data", delimiter=',')
+    dataset_name = "bupa.data"
+    bupa_data = np.loadtxt(dataset_name, delimiter=',')
 
     bupa_data = np.delete(bupa_data, 6, 1)
     # bupa_data = np.delete(bupa_data, 5, 1)
@@ -259,7 +284,7 @@ if __name__ == '__main__':
     # bupa_data = np.delete(bupa_data, 2, 1)
     # data = np.delete(data, 1, 1)
 
-    km = KMeansDE(n_clusters=2, verbose=1)
+    km = KMeansDE(n_clusters=4, verbose=1, scaling=True, dataset_name=dataset_name)
 
     km.fit_predict(bupa_data)
 
